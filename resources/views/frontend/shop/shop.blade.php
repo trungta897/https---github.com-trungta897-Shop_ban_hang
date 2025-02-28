c85f931aa0264b6b5cc9554da6c0c4dce025a48e
Nguyễn <nguyentatrunghieu2@gmail.com>
2025-02-28 16:41:15 +0700

    Changes to be committed: 	modified:   resources/views/frontend/shop/shop.blade.php

--- resources/views/frontend/shop/shop.blade.php
+++ resources/views/frontend/shop/shop.blade.php
@@ -23,44 +23,32 @@
             <div class="row">
                 <div class="col-md-3 col-sm-6">
                     @foreach ($products as $product)
-                    <div class="single-shop-product">
-                        <div class="product-upper">
-                            <img src="{{$product->image}}" alt="">
-                        </div>
-                        <h2><a href="shop/product/{{$product->id}}">{{$product->name}}</a></h2>
-                        <div class="product-carousel-price">
-                            <ins>{{$product->price}} VND</ins>
-                        </div>
+                        <div class="single-shop-product">
+                            <div class="product-upper">
+                                <img src="{{$product->image}}" alt="">
+                            </div>
+                            <h2><a href="shop/product/{{$product->id}}">{{$product->name}}</a></h2>
+                            <div class="product-carousel-price">
+                                <ins>{{$product->price}} VND</ins>
+                            </div>
 
-                        <div class="product-option-shop">
-                            <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="70" rel="nofollow" href="/canvas/shop/?add-to-cart=70">Add to cart</a>
+                            <div class="product-option-shop">
+                                <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="70"
+                                    rel="nofollow" href="/canvas/shop/?add-to-cart=70">Add to cart</a>
+                            </div>
                         </div>
-                    </div>
                     @endforeach
                 </div>
             </div>
 
             <div class="row">
-                <div class="col-md-12">
-                    <div class="product-pagination text-center">
-                        <nav>
-                          <ul class="pagination">
-                            <li>
-                              <a href="#" aria-label="Previous">
-                                <span aria-hidden="true">&laquo;</span>
-                              </a>
-                            </li>
-                            <li>{{$products->links()}}</li>
-                            <li>
-                              <a href="#" aria-label="Next">
-                                <span aria-hidden="true">&raquo;</span>
-                              </a>
-                            </li>
-                          </ul>
-                        </nav>
-                    </div>
+                <div class="col-md-12 text-center">
+                    @if (request('search') !== '' && count($products) == 0)
+                        <h1><b>Không tìm thấy sản phẩm nào</b></h1>
+                    @endif
                 </div>
             </div>
+            {{$products->links()}}
         </div>
     </div>
 
